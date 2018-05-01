e807de9a9e6bf4dc91b760f0dc41f18d16190dbb
Santi <santiboapple@gmail.com>
2018-04-30 22:52:33 -0700

    working with new API

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,11 +8,13 @@ import { Geolocation } from '@ionic-native/geolocation';
   templateUrl: 'home.html'
 })
 export class HomePage {
-	movies: Array<{title: string}>;
-	theatre: any;
+	movies: any;
+	showtimes: Array<{movie_id}>;
+	cinemas: any;
   constructor(public navCtrl: NavController, private geolocation: Geolocation, private http: HTTP) {
- 	this.movies = [];
-  	this.theatre = null;
+ 	this.movies = {};
+ 	this.cinemas = {};
+ 	this.showtimes = []
   }
   
   getGPS(){
@@ -28,14 +30,29 @@ export class HomePage {
   	this.getMovies(x)
   }
   getMovies(coords){
-  	let link = 'INT'
-  	let params = {'api_key': 'dhq97fxeenhjbj58epy5zqqf', startDate: '2018-04-28', 'lat': coords.latitude+'', 'lng': coords.longitude+''}
-  	console.log(params)
+  	let link = 'https://api.internationalshowtimes.com/v4/showtimes'
+  	let params = {'location': coords.latitude+','+coords.longitude+'', 
+  	'time_from': '2018-05-01T00:00:00-08:00', 
+  	'time_to': '2018-05-02T00:00:00-08:00', 
+  	'distance': '5',
+	'movie_fields': 'id,title,poster_image_thumbnail,ratings', 'cinema_fields': 'id,name,location.lat,location.lon,location.address.display_text',
+	'append': 'movies,cinemas'}
 
-  	this.http.get(link, params, {})
+  	this.http.get(link, params, {"X-API-Key": '31nETRjAwSnfqH88Jiq4fBqwOayPyxcz'})
 	.then(data => {
-		this.movies = JSON.parse(data.data);
-		console.log(data.data)
+		let r = JSON.parse(data.data);
+		console.log(data, r)
+		
+		for (let i of r.movies){
+			this.movies[i.id] = i
+		}
+		for (let i of r.cinemas){
+			this.cinemas[i.id] = i
+		}
+		console.log(this.movies, this.cinemas);
+		//window.movies = this.movies;
+		this.showtimes = r.showtimes;
+
 	}, error => {
 		console.log(error, this);
 	});	
@@ -46,8 +63,15 @@ export class HomePage {
   	console.log(link)
   	this.http.get(link, {'api_key': 'dhq97fxeenhjbj58epy5zqqf'}, {})
 	.then(data => {
-		this.theatre = JSON.parse(data.data);
-		console.log(data.data)
+		let r = JSON.parse(data.data);
+		console.log(data, r)
+		this.showtimes = r.showtimes;
+		for (let i of r.movies){
+			this.movies[i.id] = i
+		}
+		for (let i of r.cinemas){
+			this.cinemas[i.id] = i
+		}
 	}, error => {
 		console.log(error, this);
 	});
